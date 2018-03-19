099a61e07cbbbe7ff78088ba9ca5873a2c44b5f9
Nicholas Lester <nicholas.lester@thomsonreuters.com>
2018-03-19 15:40:03 +0000

    test

--- server/cmwell-sparql-agent/src/main/scala/cmwell/tools/data/sparql/StpUtil.scala
+++ server/cmwell-sparql-agent/src/main/scala/cmwell/tools/data/sparql/StpUtil.scala
@@ -1,7 +1,6 @@
 package cmwell.tools.data.sparql
 
 import cmwell.tools.data.utils.akka.stats.DownloaderStats.DownloadStats
-import cmwell.tools.data.utils.akka.stats.IngesterStats.IngestStats
 import cmwell.util.http.SimpleResponse.Implicits.UTF8StringHandler
 import io.circe._, io.circe.parser._
 import scala.concurrent.ExecutionContext
@@ -17,28 +16,6 @@ object StpUtil {
     val (_, name) = p.splitAt(p.lastIndexOf("/"))
     name.tail
   }
-
-  def readIngestStatistics(baseUrl: String, path: String)(implicit context : ExecutionContext) = {
-    cmwell.util.http.SimpleHttpClient.get(s"http://$baseUrl$path/stats?format=json")
-      .map(response => {
-        response.payload.lines.map({ row =>
-          parse(row) match {
-            case Left(parseFailure@ParsingFailure(_, _)) => throw parseFailure
-            case Right(json) => {
-
-              val ingestedInfotons = json.hcursor.downField("fields").downField("ingestedInfotons").downArray.as[Long].toOption
-              val failedInfotons = json.hcursor.downField("fields").downField("failedInfotons").downArray.as[Long].toOption
-
-              (ingestedInfotons, failedInfotons) match {
-                case (Some(ingested), Some(failed)) => IngestStats(ingestedInfotons=ingested, failedInfotons=failed)
-                case _ => None
-              }
-            }
-          }
-        })
-      })
-  }
-
 
   def readPreviousTokens(baseUrl: String, path: String, format: String)(implicit context : ExecutionContext) = {
     cmwell.util.http.SimpleHttpClient.get(s"http://$baseUrl$path/tokens?op=stream&recursive&format=json")
